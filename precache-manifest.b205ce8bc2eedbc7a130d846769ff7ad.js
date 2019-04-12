cadea977cd8a29d3862f637ff763c042e587766c
Ahmedfarghal <a.fegoo2@gmail.com>
2019-04-13 00:04:30 +0200

    Updates

---
+++ precache-manifest.b205ce8bc2eedbc7a130d846769ff7ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "487d51440d0b64e78d86",
+    "url": "/React-Router-dom/static/js/runtime~main.061fd85c.js"
+  },
+  {
+    "revision": "06d505e52292d99e9e5c",
+    "url": "/React-Router-dom/static/js/main.0f6b5dd5.chunk.js"
+  },
+  {
+    "revision": "422cce8a2d9c4ad07870",
+    "url": "/React-Router-dom/static/js/2.b5acb410.chunk.js"
+  },
+  {
+    "revision": "06d505e52292d99e9e5c",
+    "url": "/React-Router-dom/static/css/main.883f9cc5.chunk.css"
+  },
+  {
+    "revision": "4991ff9fc840b7ab6a79089ef8b663b4",
+    "url": "/React-Router-dom/index.html"
+  }
+];
